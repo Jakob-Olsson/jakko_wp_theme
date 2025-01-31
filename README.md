ceeeccbb3cd745036df6f1b880da72a8170f185c
jakob-olsson <jakobolsson13@gmail.com>
2025-01-31 15:24:16 +0100

    Added screenshot, as well as enqueues for stylesheets and main script file.

---
+++ README.md
@@ -0,0 +1,1 @@
+- WordPress Theme - Jakko_standard
--- functions.php
+++ functions.php
@@ -6,3 +6,19 @@
  *  @package jakko_standard
  * 
  */
+
+function jakko_standard_equeue_scripts() {
+    // Register and enqueue the main stylesheet
+    wp_register_style('style-css', get_stylesheet_uri(), [], filemtime( get_template_directory().'/style.css'), 'all' );
+    wp_enqueue_style( 'style-css' );
+
+    // Register and enqueue the main JavaScript file
+    wp_register_script('main-js', get_template_directory_uri().'/assets/main.js', [], filemtime( get_template_directory().'/assets/main.js'), true );
+    wp_enqueue_script( 'main-js' );
+
+
+}
+// Load scripts and styles in WordPress
+add_action('wp_enqueue_scripts', 'jakko_standard_equeue_scripts');
+
+?>
